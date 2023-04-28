93a399fb8ce1180fc1e71618f0c12088a046ea0e
michaelagejew <magejew@uncc.edu>
2023-04-28 14:25:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,27 @@ chart = alt.Chart(filtered_df).mark_line().encode(
 )
 
 st.altair_chart(chart, use_container_width=True)
+
+
+df['bin'] = pd.cut(df['mass (g)'], bins=[0, 1, 5, 10, 50, 100, 500, 1000, 10000, 100000, 1000000, 1000000000])
+
+#group the pieces together by count
+df2 = df.groupby(['bin'])['bin'].count().reset_index(name="count")
+
+# pretty colors
+my_colors = [(x/10.0, x/20.0, 0.75) for x in range(len(df))]
+
+#print horizontal bar chart (using colors above)
+#df2.plot.barh(x='bin', 
+ #             y='count', 
+  #            stacked=True, 
+   #           color=my_colors, 
+    #          title='Meteorite Landings by Mass (g)', 
+     #         xlabel='Number of landings recorded by NASA',
+      #        ylabel='Mass of Meteorite',
+       #       figsize=(12, 4))
+chart = alt.Chart(df2).mark_bar().encode(
+ y = 'bin',
+ x= 'count'
+)
+st.altair_chart(chart, use_container_width = True)
