668a0efbe938c9dbc0ad0c1d0369bb09cd766f46
michaelagejew <magejew@uncc.edu>
2023-04-28 15:13:32 -0400

    Update map.py

--- map.py
+++ map.py
@@ -4,7 +4,6 @@ import numpy as np
 import streamlit as st
 df = pd.read_csv('Meteorite_Landings.csv')
 alt.data_transformers.disable_max_rows()
-df
 url = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-continents.json"
 source = alt.topo_feature(url, "continent")
 
