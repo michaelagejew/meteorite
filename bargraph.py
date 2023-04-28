e5e3ad249be10b82993bfbd97135d54eced50f46
michaelagejew <magejew@uncc.edu>
2023-04-28 14:20:03 -0400

    Update bargraph.py

--- bargraph.py
+++ bargraph.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import altair as alt
 
 pd.read_csv('Meteorite_Landings.csv')
 df = pd.read_csv("Meteorite_Landings.csv", usecols = ['mass (g)','name'])
@@ -23,4 +24,8 @@ my_colors = [(x/10.0, x/20.0, 0.75) for x in range(len(df))]
      #         xlabel='Number of landings recorded by NASA',
       #        ylabel='Mass of Meteorite',
        #       figsize=(12, 4))
-st.bar_chart(df2)
+chart = alt.Chart(df2).mark_bar().encode(
+ y = 'mass (g)',
+ x= 'count'
+)
+st.altair_chart(bar_chart, use_container_width = True)
