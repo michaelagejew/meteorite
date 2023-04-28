d4bccbed0bdb8998c1c34ba68779199d0c6d3a55
michaelagejew <magejew@uncc.edu>
2023-04-28 13:29:25 -0400

    Update map.py

--- map.py
+++ map.py
@@ -1,7 +1,6 @@
 import altair as alt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns 
 df = pd.read_csv('/content/Meteorite_Landings.csv')
 alt.data_transformers.disable_max_rows()
